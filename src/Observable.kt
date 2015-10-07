0cba6bf3674e5cde04a93da5226717c93873678e
Roman Belkov <romanbelkov@gmail.com>
2015-10-07 19:07:42 +0300

    New Map function for Observables

--- src/Observable.kt
+++ src/Observable.kt
@@ -21,3 +21,46 @@ fun <T> Observable<T>.Subscribe(f: (T) -> Unit): Closeable = this.Subscribe(
             return f(t)
         }
     })
+
+/*let interval(timeSpan: System.TimeSpan) =
+        let counter = ref 0
+        let notifier = new Subject<int>()
+        let cts = new CancellationTokenSource()
+
+        let rec triggering (token: CancellationToken) =
+            async {
+                if not token.IsCancellationRequested then
+                    notifier.OnNext !counter
+                    incr counter
+                    do! Async.Sleep(int timeSpan.Ticks/10000)
+                    return! triggering token
+                  }
+        triggering cts.Token |> Async.Start
+        notifier :> IObservable<_>
+   */
+
+//fun <T> Observable<T>.Interval() {}
+
+private fun <A, B> protect(f: () -> A, succeed: (A) -> B, fail: (Throwable) -> B): B {
+    try {
+        val v = f()
+        return succeed(v)
+    } catch (e: Throwable) {
+        return fail(e)
+    }
+}
+
+fun <T, E> Observable<T>.Map(f: (T) -> E, observable: Observable<T>) = object : Observable<E> {
+    override fun Subscribe(observer: Observer<E>): Closeable = object : BasicObserver<T>() {
+
+        override fun Next(value: T) = protect({f(value)}, {x -> observer.onNext(x)}, {x -> observer.onError(x)})
+
+        override fun Completed() = observer.onCompleted()
+
+        override fun Error(e: Throwable) = observer.onError(e)
+
+        override fun close() = Completed()
+
+    }
+}
+
--- src/Observer.kt
+++ src/Observer.kt
@@ -1,3 +1,5 @@
+import java.io.Closeable
+
 /**
  * Created by Roman Belkov on 06.10.15.
  */
@@ -6,10 +8,32 @@ interface Observer<T> {
 
     fun onCompleted()
 
-
     fun onError(e: Throwable)
 
-
     fun onNext(t: T)
+}
 
+abstract class BasicObserver<T> : Observer<T>, Closeable {
+    var stopped = false
+    abstract fun Next(value: T): Unit
+    abstract fun Error(e: Throwable): Unit
+    abstract fun Completed(): Unit
+
+    override fun onNext(t: T) {
+        if(!stopped) Next(t)
+    }
+
+    override fun onError(e: Throwable) {
+        if(!stopped) {
+            stopped = true
+            Error(e)
+        }
+    }
+
+    override fun onCompleted() {
+        if(!stopped) {
+            stopped = true
+            Completed()
+        }
+    }
 }
