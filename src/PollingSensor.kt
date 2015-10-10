1342ae271d435114bfaa1954b97cc3de8e215c5e
Roman Belkov <romanbelkov@gmail.com>
2015-10-10 13:32:43 +0300

    PollingSensor is now using RxJava Observables

--- src/PollingSensor.kt
+++ src/PollingSensor.kt
@@ -1,8 +1,15 @@
+import rx.Observable
+import java.util.concurrent.TimeUnit
+
 /**
  * Created by Roman Belkov on 18.08.2015.
  */
 
 abstract class PollingSensor {
-    val defaultRate = 50
+    val defaultRate: Long = 50
     abstract fun Read() : Int
+
+    fun ToObservable(pollingRate: Long, unit: TimeUnit) = Observable.interval(pollingRate, unit).map { Read() }
+
+    fun ToObservable() = ToObservable(defaultRate, TimeUnit.MILLISECONDS)
 }
