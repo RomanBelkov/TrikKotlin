7b373b268040e562009f553ef83e9ed1daf7cabc
Roman Belkov <romanbelkov@gmail.com>
2015-10-17 21:19:08 +0300

    Corrected 'ReadVoltage' in Battery class

--- src/Battery.kt
+++ src/Battery.kt
@@ -1,10 +1,10 @@
 /**
 * Created by Roman Belkov on 14.09.15.
 */
-public object Battery: PollingSensor() {
+public class Battery: PollingSensor() {
     override fun Read(): Int = I2cTrik.ReadWord(0x26)
 
     fun ReadVoltage(): Double {
-        return this.Read() / 13560.577 // 1023.0 * 3.3 * (7.15 + 2.37) / 2.37
+        return (this.Read().toDouble() / 1023.0) * 13.255696203   // 3.3 * (7.15 + 2.37) / 2.37
     }
 }
